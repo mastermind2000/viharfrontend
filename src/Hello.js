aa81fde58b02391e99e68d4dc8cebc068f422757
mastermind2000 <31075822+mastermind2000@users.noreply.github.com>
2021-05-11 23:11:26 +0530

    removed Select Policy

--- src/Hello.js
+++ src/Hello.js
@@ -1,3 +1,23 @@
-import React from 'react';
+import React from "react";
 
 export default ({ name }) => <h1>Hello {name}!</h1>;
+<Grid item xs={12}>
+  <Condition when="role" is="student">
+    <Field
+      fullWidth
+      name="policy"
+      component={Select}
+      label="Select Policies"
+      formControlProps={{ fullWidth: true }}
+    >
+      <MenuItem value="Policy1">
+        {" "}
+        Jupyter Notebook of instance type ml.t2.medium for 60 mins duration
+        Jupyter Notebook of instance type ml.t2.large for 30 mins duration
+        Processing Job of instance ml.t3.medium with instance count 1 and
+        additional volume for storage 1GB Training Job of instance ml.t3.medium
+        with instance count 1 and additional volume for storage 1GB
+      </MenuItem>
+    </Field>
+  </Condition>
+</Grid>;
--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -183,28 +183,7 @@ export default function Home() {
                     </Field>
                   </Condition>
                 </Grid>
-                <Grid item xs={12}>
-                  <Condition when="role" is="student">
-                    <Field
-                      fullWidth
-                      name="policy"
-                      component={Select}
-                      label="Select Policies"
-                      formControlProps={{ fullWidth: true }}
-                    >
-                      <MenuItem value="Policy1">
-                        {" "}
-                        Jupyter Notebook of instance type ml.t2.medium for 60
-                        mins duration Jupyter Notebook of instance type
-                        ml.t2.large for 30 mins duration Processing Job of
-                        instance ml.t3.medium with instance count 1 and
-                        additional volume for storage 1GB Training Job of
-                        instance ml.t3.medium with instance count 1 and
-                        additional volume for storage 1GB
-                      </MenuItem>
-                    </Field>
-                  </Condition>
-                </Grid>
+
                 <Grid item xs={12}>
                   <Condition when="group" is="Student">
                     <table>

d13f724908360a1994d79e6afe67d8087e0e79aa
mastermind2000 <31075822+mastermind2000@users.noreply.github.com>
2021-06-05 03:13:28 +0530

    usage done

--- src/pages/Usage/sauvegarde.js
+++ src/pages/Usage/sauvegarde.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import Axios from "axios";
+import React, { useState, useEffect } from "react";
+import ReactDOM, { render } from "react-dom";
+import axios from "axios";
 import { Form, Field } from "react-final-form";
 import { TextField, Checkbox, Radio, Select } from "final-form-material-ui";
 import {
@@ -18,9 +18,39 @@ import {
   FormControlLabel
 } from "@material-ui/core";
 // Picker
-function callApi() {}
+
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 
-const Sauvegarde = () => <Fragment>To be done</Fragment>;
+const Sauvegarde = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [item, setItem] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
+  const getData = () => {
+    const url =
+      "https://4veg3aetvd.execute-api.us-east-2.amazonaws.com/dev/list";
+    axios(url)
+      .then((response) => {
+        setIsLoading(false);
+        console.log("RES", response.data);
+        setItem(response.data);
+        console.log("TEST", item);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.log("Error", error);
+      });
+  };
+  const content = isLoading ? (
+    <div> Processing.... </div>
+  ) : (
+    <div>
+      <pre> {JSON.stringify(item, null, 2)} </pre>
+    </div>
+  );
+
+  return <div> {content} </div>;
+};
 
 export default Sauvegarde;

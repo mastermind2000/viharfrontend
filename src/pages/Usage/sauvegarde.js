b9dae74beeca1eaac8b667daf17ee3d327e6155b
mastermind2000 <31075822+mastermind2000@users.noreply.github.com>
2021-06-06 15:47:26 +0530

    css

--- src/pages/Usage/sauvegarde.js
+++ src/pages/Usage/sauvegarde.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import ReactDOM, { render } from "react-dom";
 import axios from "axios";
 import { Form, Field } from "react-final-form";
+import ReactTable from "react-table";
+//import '../../react-table/react-table.css';
 import { TextField, Checkbox, Radio, Select } from "final-form-material-ui";
 import {
   Typography,
@@ -20,14 +22,16 @@ import {
 // Picker
 
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-function master() {}
+import "./table.css";
+
 const Sauvegarde = () => {
   const [data, setData] = useState([]);
   const [trigger, setTrigger] = useState(false);
   const url = "https://4veg3aetvd.execute-api.us-east-2.amazonaws.com/dev/list";
   const handleClick = () => {
     axios.get(url).then((json) => setData(json.data));
-    console.log(data.length);
+
+    console.log(data);
   };
   const renderTable = () => {
     return (
@@ -46,28 +50,46 @@ const Sauvegarde = () => {
   };
   const master = () => {
     return (
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Arn</th>
-              <th>Name</th>
-              <th>Status</th>
-              <th>Time Left</th>
-            </tr>
-          </thead>
-          <tbody>{renderTable()}</tbody>
-        </table>
-      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Arn</th>
+            <th>Name</th>
+            <th>Status</th>
+            <th>Time Left</th>
+          </tr>
+        </thead>
+        <tbody>{renderTable()}</tbody>
+      </table>
     );
   };
-
+  const secondary = () => {
+    const columns = [
+      {
+        Header: "ARN",
+        ancessor: "arn"
+      },
+      {
+        Header: "Instance Name",
+        ancessor: "iname"
+      },
+      {
+        Header: "Status",
+        ancessor: "statud"
+      },
+      {
+        Header: "Time Left",
+        ancessor: "tl"
+      }
+    ];
+    return <ReactTable data={data} columns={columns} />;
+  };
   return (
     <React.Fragment>
       <div>
         <button onClick={handleClick}>Load</button>
       </div>
-      <div>{data.length > 0 && master()}</div>
+      <div id="app">{data.length > 0 && master()}</div>
     </React.Fragment>
   );
 };

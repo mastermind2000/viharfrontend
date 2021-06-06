5040fa90c13ed24db39e83cc0d9229c6f490901e
mastermind2000 <31075822+mastermind2000@users.noreply.github.com>
2021-06-07 01:37:21 +0530

    minor tweaks

--- src/pages/About/AboutPage.js
+++ src/pages/About/AboutPage.js
@@ -1,9 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactDOM, { render } from "react-dom";
+import axios from "axios";
+import { Form, Field } from "react-final-form";
+import ReactTable from "react-table";
+//import '../../react-table/react-table.css';
+import { TextField, Checkbox, Radio, Select } from "final-form-material-ui";
+import {
+  Typography,
+  Paper,
+  Link,
+  Grid,
+  Button,
+  CssBaseline,
+  RadioGroup,
+  FormLabel,
+  MenuItem,
+  FormGroup,
+  FormControl,
+  FormControlLabel
+} from "@material-ui/core";
+// Picker
 
-const About = () => (
-  <div>
-    <h1> To be decided </h1>
-  </div>
-);
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import "./table.css";
+
+const About = () => {
+  const [data, setData] = useState([]);
+  const [trigger, setTrigger] = useState(false);
+  const url = "https://4veg3aetvd.execute-api.us-east-2.amazonaws.com/dev/user";
+  const handleClick = () => {
+    axios.get(url).then((json) => setData(json.data));
+
+    console.log(data);
+  };
+  const renderTable = () => {
+    return (
+      data &&
+      data.map((instance) => {
+        return (
+          <tr>
+            <td>{instance.email}</td>
+            <td>{instance.name}</td>
+            <td>{instance.group}</td>
+          </tr>
+        );
+      })
+    );
+  };
+  const master = () => {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Email</th>
+            <th>Name</th>
+            <th>Group</th>
+          </tr>
+        </thead>
+        <tbody>{renderTable()}</tbody>
+      </table>
+    );
+  };
+  const secondary = () => {
+    const columns = [
+      {
+        Header: "ARN",
+        ancessor: "arn"
+      },
+      {
+        Header: "Instance Name",
+        ancessor: "iname"
+      },
+      {
+        Header: "Status",
+        ancessor: "statud"
+      },
+      {
+        Header: "Time Left",
+        ancessor: "tl"
+      }
+    ];
+    return <ReactTable data={data} columns={columns} />;
+  };
+  return (
+    <React.Fragment>
+      <div>
+        <button onClick={handleClick}>Load</button>
+      </div>
+      <div id="app">{data.length > 0 && master()}</div>
+    </React.Fragment>
+  );
+};
 
 export default About;
